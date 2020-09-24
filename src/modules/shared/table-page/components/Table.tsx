31c93449b10d638216c059c7ec9fb3c11cb0f769
chico <chico.zimcolor@gmail.com>
2020-09-24 16:03:16 -0300

    fixed missing node keys on Table

--- src/modules/shared/table-page/components/Table.tsx
+++ src/modules/shared/table-page/components/Table.tsx
@@ -9,11 +9,11 @@ interface TableProps {
 }
 
 export default function Table(props: TableProps) {
-    const headerRow = () => tableRow(mapHeaders())
-    const tableBody = () => mapItems()
+    const headerRow = () => tableRow(mapHeaders(), 'header')
+    const tableBody = () => makeBody()
 
-    const tableRow = (content: ReactNode[]) => 
-        <tr>{content}</tr>
+    const tableRow = (content: ReactNode[], key: string) => 
+        <tr key={key}>{content}</tr>
 
     const mapHeaders = () => 
         props.headers.map((header) => 
@@ -22,14 +22,19 @@ export default function Table(props: TableProps) {
             </th>
         )
     
-    const mapItems = () => 
-        props.data.map(item => 
-            tableRow(mapAttributes(item))
-        )
+    const makeBody = () => {
+        let count = 0
+        return props.data.map(item => {
+            let key = 'datarow-' + count.toString()
+            count ++
+            return tableRow(mapAttributes(item), key)
+        })
+    }
+
     
     const mapAttributes = (item: any) => 
         props.headers.map(attr => 
-            <td>{item[attr]}</td>
+            <td key={attr}>{item[attr]}</td>
         )
 
     return (
